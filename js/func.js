b7a66499a972cbf8d1cbbd17dedca67efe2a9161
morionguesthouse <morion.guesthouse@gmail.com>
2016-11-23 01:18:08 +0800

    Update func.js

--- js/func.js
+++ js/func.js
@@ -25,3 +25,8 @@ $(document).ready(function(){
     });
 
 });
+$(document).on('click','.navbar-collapse.in',function(e) {
+    if( $(e.target).is('a') ) {
+        $(this).collapse('hide');
+    }
+});
